1a54e2ba512c7792c8b6ffe8823cc0f2a59a930e
ibrahima <ibrahima.sr.sow@gmail.com>
2024-08-10 19:34:57 -0400

    remove monitor task

--- flight/sm_configuration.py
+++ flight/sm_configuration.py
@@ -1,5 +1,4 @@
 from tasks.imu import Task as imu
-from tasks.monitor import Task as monitor
 from tasks.obdh import Task as obdh
 from tasks.radio_comms import Task as comms
 from tasks.sun import Task as sun
@@ -11,15 +10,14 @@ TODO Copy the state descriptions here
 
 """
 
-TASK_REGISTRY = {"MONITOR": monitor, "TIMING": timing, "OBDH": obdh, "IMU": imu, "SUN": sun, "COMMS": comms}
+TASK_REGISTRY = {"TIMING": timing, "OBDH": obdh, "IMU": imu, "SUN": sun, "COMMS": comms}
 
-TASK_MAPPING_ID = {"MONITOR": 0x05, "TIMING": 0x01, "OBDH": 0x02, "IMU": 0x03, "SUN": 0x11, "COMMS": 0x12}
+TASK_MAPPING_ID = {"TIMING": 0x01, "OBDH": 0x02, "IMU": 0x03, "SUN": 0x11, "COMMS": 0x12}
 
 
 SM_CONFIGURATION = {
     "STARTUP": {
         "Tasks": {
-            "MONITOR": {"Frequency": 1, "Priority": 1},
             "TIMING": {"Frequency": 1, "Priority": 2},
             "OBDH": {"Frequency": 1, "Priority": 3},
         },
@@ -27,7 +25,6 @@ SM_CONFIGURATION = {
     },
     "NOMINAL": {
         "Tasks": {
-            "MONITOR": {"Frequency": 1, "Priority": 1},
             "TIMING": {"Frequency": 1, "Priority": 2},
             "OBDH": {"Frequency": 1, "Priority": 2},
             "IMU": {"Frequency": 1, "Priority": 5, "ScheduleLater": True},
@@ -38,7 +35,6 @@ SM_CONFIGURATION = {
     },
     "DOWNLINK": {
         "Tasks": {
-            "MONITOR": {"Frequency": 1, "Priority": 1},
             "OBDH": {"Frequency": 1, "Priority": 2},
             "IMU": {"Frequency": 1, "Priority": 3},
             "COMMS": {"Frequency": 0.1, "Priority": 5},
@@ -47,7 +43,6 @@ SM_CONFIGURATION = {
     },
     "LOW_POWER": {
         "Tasks": {
-            "MONITOR": {"Frequency": 1, "Priority": 1},
             "OBDH": {"Frequency": 1, "Priority": 2},
             "IMU": {"Frequency": 2, "Priority": 3},
         },
@@ -55,7 +50,6 @@ SM_CONFIGURATION = {
     },
     "SAFE": {
         "Tasks": {
-            "MONITOR": {"Frequency": 1, "Priority": 1},
             "OBDH": {"Frequency": 1, "Priority": 2},
             "IMU": {"Frequency": 2, "Priority": 3},
         },
