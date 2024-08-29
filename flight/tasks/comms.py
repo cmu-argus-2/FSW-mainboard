d624d01868775ac9ad7066f47c36e6f5189e1ac0
Akshat412 <akshat412000@gmail.com>
2024-08-29 13:13:41 -0400

    Fix for comms transmissions

--- flight/tasks/comms.py
+++ flight/tasks/comms.py
@@ -17,12 +17,10 @@ class Task(TemplateTask):
             # Pack telemetry at the task rate
 
             if TelemetryPacker.TM_AVAILABLE:
-                SATELLITE_RADIO.tm_frame = TelemetryPacker.FRAME()
+                SATELLITE_RADIO.tm_frame = bytearray([0x01, 0x00, 0x01, 0x04, 0xFF, 0xEE, 0xDD, 0xCC])
 
                 # Transmit telemetry
                 self.tx_id = SATELLITE_RADIO.transmit_message()
                 self.log_info(
-                    f"Sent message with ID: {self.tx_id} \
-                    - SEQ_COUNT: {TelemetryPacker.SEQ_COUNT()} \
-                    - PACKET LENGTH: {TelemetryPacker.PACKET_LENGTH()} bytes"
+                    f"Sent message with ID: {self.tx_id} - SEQ_COUNT: {SATELLITE_RADIO.tm_frame[1:3]} - PACKET LENGTH: {SATELLITE_RADIO.tm_frame[3]} bytes"
                 )
