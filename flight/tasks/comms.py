3d0ea27edeb044920d4c48bebb854b0be1b4efdf
Akshat412 <akshat412000@gmail.com>
2024-09-26 14:50:33 -0400

    Blocking task prototype for comms

--- flight/tasks/comms.py
+++ flight/tasks/comms.py
@@ -10,23 +10,33 @@ from core.states import STATES
 class Task(TemplateTask):
     tx_msg_id = 0x00
     rq_msg_id = 0x00
+    msg_cnt = 0
+    ground_pass = True
 
     async def main_task(self):
 
         if SM.current_state == STATES.NOMINAL:
 
             # Pack telemetry at the task rate
+            while self.msg_cnt < 8 and self.ground_pass is True:
+                if TelemetryPacker.TM_AVAILABLE:
+                    SATELLITE_RADIO.tm_frame = TelemetryPacker.FRAME()
 
-            if TelemetryPacker.TM_AVAILABLE:
-                SATELLITE_RADIO.tm_frame = TelemetryPacker.FRAME()
+                    # Transmit telemetry
+                    self.tx_msg_id = SATELLITE_RADIO.transmit_message()
+                    self.log_info(f"Sent message with ID: {self.tx_msg_id}")
 
-                # Transmit telemetry
-                self.tx_msg_id = SATELLITE_RADIO.transmit_message()
-                self.log_info(f"Sent message with ID: {self.tx_msg_id}")
+                    self.rq_msg_id = SATELLITE_RADIO.receive_message()
 
-                self.rq_msg_id = SATELLITE_RADIO.receive_message()
+                    if self.rq_msg_id != 0x00:
+                        self.log_info(f"GS requested message ID: {self.rq_msg_id}")
+                        self.msg_cnt += 1
+                        self.ground_pass = True
+                    else:
+                        self.log_info("No response from GS")
+                        self.ground_pass = False
+                        self.msg_cnt = 0
 
-                if self.rq_msg_id != 0x00:
-                    self.log_info(f"GS requested message ID: {self.rq_msg_id}")
-                else:
-                    self.log_info("No response from GS")
+            # Reset variables
+            self.msg_cnt = 0
+            self.ground_pass = True
