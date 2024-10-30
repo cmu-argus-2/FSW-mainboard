08f1f85332518c7355657a68e1bbb9a9ab74ded4
Akshat412 <akshat412000@gmail.com>
2024-10-30 14:20:55 -0400

    Fixed build tools

--- flight/hal_test.py
+++ flight/hal_test.py
@@ -1,9 +1,9 @@
-# from hal.configuration import SATELLITE
+from hal.configuration import SATELLITE
 
-# # ---------- MAIN CODE STARTS HERE! ---------- ##
-# boot_errors = SATELLITE.boot_sequence()
-# print("ARGUS-1 booted.")
-# print(f"Boot Errors: {boot_errors}")
+# ---------- MAIN CODE STARTS HERE! ---------- ##
+boot_errors = SATELLITE.boot_sequence()
+print("ARGUS-1 booted.")
+print(f"Boot Errors: {boot_errors}")
 
 # # Board Power Monitor Test
 # # while True:
@@ -15,25 +15,25 @@
 # #     if SATELLITE.CHARGER is not None:
 # #         print(SATELLITE.CHARGER.charger_status1())
 
-# # imu test
-# while True:
-#     if SATELLITE.IMU is not None:
-#         print(SATELLITE.IMU.gyro())
-
-
-import time
-import board, busio
-from hal.drivers.bmx160 import BMX160
-
-# set up BMX160 through I2C # note: also supports SPI communication through the BMX160_SPI class
-i2c = busio.I2C(board.SCL1, board.SDA1) 
-bmx = BMX160(i2c, 0x69)
-
-# conservative warm-up time
-time.sleep(0.1) 
-
+# imu test
 while True:
-    # Just call e.g. bmx.gyro to read the gyro value
-    print("gyroscope:", bmx.gyro())
-    print("accelerometer:", bmx.accel())
-    print("magnetometer:", bmx.mag())
+    if SATELLITE.IMU is not None:
+        print(SATELLITE.IMU.gyro())
+
+
+# import time
+# import board, busio
+# from hal.drivers.bmx160 import BMX160
+
+# # set up BMX160 through I2C # note: also supports SPI communication through the BMX160_SPI class
+# i2c = busio.I2C(board.SCL1, board.SDA1) 
+# bmx = BMX160(i2c, 0x69)
+
+# # conservative warm-up time
+# time.sleep(0.1) 
+
+# while True:
+#     # Just call e.g. bmx.gyro to read the gyro value
+#     print("gyroscope:", bmx.gyro())
+#     print("accelerometer:", bmx.accel())
+#     print("magnetometer:", bmx.mag())
