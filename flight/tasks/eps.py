29407287153aa23334f566f4b52122ca23806e8b
Ankita Chatterjee <achstar1102004@gmail.com>
2025-01-23 21:45:43 -0500

    Minor formatting changes

--- flight/tasks/eps.py
+++ flight/tasks/eps.py
@@ -78,14 +78,14 @@ class Task(TemplateTask):
         # read values from MAX17205
         fuel_gauge = SATELLITE.FUEL_GAUGE
         if (fuel_gauge is not None):
-            # self.log_data[EPS_IDX.BATTERY_PACK_REPORTED_SOC] = int(fuel_gauge.read_soc())
-            # self.log_data[EPS_IDX.BATTERY_PACK_REPORTED_CAPACITY] = int(fuel_gauge.read_capacity())
-            # self.log_data[EPS_IDX.BATTERY_PACK_CURRENT] = int(fuel_gauge.read_current())
-            # self.log_data[EPS_IDX.BATTERY_PACK_VOLTAGE] = int(fuel_gauge.read_voltage())
-            # self.log_data[EPS_IDX.BATTERY_PACK_MIDPOINT_VOLTAGE] = int(fuel_gauge.read_midvoltage())
+            self.log_data[EPS_IDX.BATTERY_PACK_REPORTED_SOC] = int(fuel_gauge.read_soc())
+            self.log_data[EPS_IDX.BATTERY_PACK_REPORTED_CAPACITY] = int(fuel_gauge.read_capacity())
+            self.log_data[EPS_IDX.BATTERY_PACK_CURRENT] = int(fuel_gauge.read_current())
+            self.log_data[EPS_IDX.BATTERY_PACK_VOLTAGE] = int(fuel_gauge.read_voltage())
+            self.log_data[EPS_IDX.BATTERY_PACK_MIDPOINT_VOLTAGE] = int(fuel_gauge.read_midvoltage())
             self.log_data[EPS_IDX.BATTERY_CYCLES] = int(fuel_gauge.read_cycles())
-            self.log_data[EPS_IDX.BATTERY_PACK_TTE] = int(fuel_gauge.read_tte())
-            self.log_data[EPS_IDX.BATTERY_PACK_TTF] = int(fuel_gauge.read_ttf())
+            # self.log_data[EPS_IDX.BATTERY_PACK_TTE] = int(fuel_gauge.read_tte())
+            # self.log_data[EPS_IDX.BATTERY_PACK_TTF] = int(fuel_gauge.read_ttf())
             self.log_data[EPS_IDX.BATTERY_TIME_SINCE_POWER_UP] = int(fuel_gauge.read_time_pwrup())
 
     async def main_task(self):
@@ -96,7 +96,7 @@ class Task(TemplateTask):
         else:
 
             if not DH.data_process_exists("eps"):
-                data_format = "Lhhb" + "h" * 5 +  "i" * 2 + "h" * 31 # - use mV for voltage and mA for current (h = short integer 2 bytes, i = 4 bytes)
+                data_format = "Lhhb" + "h" * 5 +  "L" * 2 + "h" * 31 # - use mV for voltage and mA for current (h = short integer 2 bytes, i = 4 bytes)
                 DH.register_data_process("eps", data_format, True, data_limit=100000)
 
             # Get power system readings
@@ -124,7 +124,6 @@ class Task(TemplateTask):
                     )
 
             self.read_fuel_gauge()
-            DH.log_data("eps", self.log_data)
             self.log_info(f"Battery Pack Reported SOC: {self.log_data[EPS_IDX.BATTERY_PACK_REPORTED_SOC]}% ")
             self.log_info(f"Battery Pack Reported Capacity: {self.log_data[EPS_IDX.BATTERY_PACK_REPORTED_CAPACITY]} mAh ")
             self.log_info(f"Battery Pack Current: {self.log_data[EPS_IDX.BATTERY_PACK_CURRENT]} mA ")
@@ -134,3 +133,4 @@ class Task(TemplateTask):
             self.log_info(f"Battery Pack Time-to-Empty: {self.log_data[EPS_IDX.BATTERY_PACK_TTE]} seconds ")
             self.log_info(f"Battery Pack Time-to-Full {self.log_data[EPS_IDX.BATTERY_PACK_TTF]} seconds ")
             self.log_info(f"Battery Pack Time Since Power Up {self.log_data[EPS_IDX.BATTERY_TIME_SINCE_POWER_UP]} seconds ")
+            DH.log_data("eps", self.log_data)
