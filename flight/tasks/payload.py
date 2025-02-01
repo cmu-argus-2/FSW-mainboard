4f2043ac1b0847f030b51688d27292823754187a
ibrahima <ibrahima.sr.sow@gmail.com>
2025-02-01 10:41:05 -0800

    fixes to DH img process for .jpg format + other interfaces

--- flight/tasks/payload.py
+++ flight/tasks/payload.py
@@ -1,6 +1,9 @@
 # Payload Control Task
 
 from core import TemplateTask
+from core import state_manager as SM
+from core.data_handler import DataHandler as DH
+from core.states import STATES
 
 
 class Task(TemplateTask):
@@ -9,4 +12,12 @@ class Task(TemplateTask):
         self.name = "PAYLOAD"
 
     async def main_task(self):
-        pass
+
+        if SM.current_state == STATES.STARTUP:
+            pass
+        else:
+
+            if not DH.data_process_exists("img"):
+                DH.register_image_process()  # WARNING: Image process from DH is different from regular data processes!
+
+            # TODO: other commands and logging here
