3ae77eaf29f0d2d9596783798a2087c163852bd3
Alena Lu <alena.jiawen@gmail.com>
2025-02-20 16:23:37 -0500

    added ci work

---
+++ sil_run.py
@@ -0,0 +1,21 @@
+import subprocess
+import time
+import signal
+import os
+from datetime import datetime
+
+timestamp = datetime.now().strftime("%m-%d_%H-%M-%S")
+log_filename = f"{timestamp}_sim.log"
+# error_log_filename = f"{timestamp}_err.log"
+
+try:
+    with open(log_filename, "w") as log_file:
+        process = subprocess.Popen(["./run.sh", "simulate"], stdout=log_file, stderr=log_file, preexec_fn=os.setsid)
+        print(f"Running simulation for 120 seconds, output written to {log_filename}")
+        time.sleep(30)
+        print("terminating...")
+        os.killpg(os.getpgid(process.pid), signal.SIGKILL)
+        print("done")
+
+except Exception as e:
+    print(f"Error: {e}")
