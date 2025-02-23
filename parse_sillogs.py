bb1073ba3240d08c2639cd54e6195722822285a4
Alena Lu <alena.jiawen@gmail.com>
2025-02-23 14:50:07 -0500

    fixed parse logs err

--- parse_sillogs.py
+++ parse_sillogs.py
@@ -2,6 +2,7 @@ log_filename = "sil_sim.log"
 error_filename = "sil_err.log"
 
 python_err = False
+sim_err = False
 with open(log_filename, "r") as log_file, open(error_filename, "w") as error_log:
     for line in log_file:
         # errors running the SIL
