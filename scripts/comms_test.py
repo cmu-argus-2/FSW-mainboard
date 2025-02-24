ca61e978b59323a682497655d4c893e80b391a2a
Akshat412 <akshat412000@gmail.com>
2025-02-24 13:37:40 -0500

    Changed frequency to 433 MHz

--- scripts/comms_test.py
+++ scripts/comms_test.py
@@ -1,7 +1,13 @@
 from hal.configuration import SATELLITE
 
+print()
+print("Booting ARGUS-1...")
+boot_errors = SATELLITE.boot_sequence()
+print("ARGUS-1 booted.")
+print(f"Boot Errors: {boot_errors}")
+
 # Forever loop that just transmits to the GS, no receive
 while True:
-    tx_message = bytes([0xFF, 0x00, 0x01, 0xF0]) + bytearray(240)
+    tx_message = bytes([0xFF, 0x00, 0x01, 0xC8]) + bytearray(200)
     SATELLITE.RADIO.send(tx_message)
     print("Sent message to GS")
