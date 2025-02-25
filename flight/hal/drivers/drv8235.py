86b97fdc3e1dec764b64d43ed2a8ded7b1f6a39d
Perrin <perrintong@cmu.edu>
2025-02-25 17:12:12 -0500

    linting

--- flight/hal/drivers/drv8235.py
+++ flight/hal/drivers/drv8235.py
@@ -1,33 +1,31 @@
-import sys
-import busio
-from board import *
-#sys.path.append("/home/sebastian/FSW-mainboard/flight")
+# sys.path.append("/home/sebastian/FSW-mainboard/flight")
 from adafruit_bus_device.i2c_device import I2CDevice
 from adafruit_register.i2c_bit import ROBit, RWBit
-from adafruit_register.i2c_bits import RWBits, ROBits
-#from hal.drivers.middleware.errors import Errors
-#from hal.drivers.middleware.generic_driver import Driver
+from adafruit_register.i2c_bits import ROBits, RWBits
+
+# from hal.drivers.middleware.errors import Errors
+# from hal.drivers.middleware.generic_driver import Driver
 from micropython import const
 
 # DEVICE REGISTER MAP
-_FAULT_STATUS = const(0x00)   # Fault Status Register R-
-_RC_STATUS1   = const(0x01)   # Motor Speed status R-
-_REG_STATUS1  = const(0x04)   # Output voltage across motor R-
-_REG_STATUS2  = const(0x05)   # Current flowing through motor R-
-_REG_STATUS3  = const(0x06)   # PWM Duty Cycle R-
-_CONFIG0      = const(0x09)   # RW
-_CONFIG1      = const(0x0A)   # RW
-_CONFIG2      = const(0x0B)   # RW
-_CONFIG3      = const(0x0C)   # RW
-_CONFIG4      = const(0x0D)   # RW
-_REG_CTRL0    = const(0x0E)   # RW
-_REG_CTRL1    = const(0x0F)   # RW
-_REG_CTRL2    = const(0x10)   # RW
-_RC_CTRL2     = const(0x13)   # RW
-_RC_CTRL3     = const(0x14)   # RW
-_RC_CTRL4     = const(0x15)   # RW
-_RC_CTRL7     = const(0x18)   # RW
-_RC_CTRL8     = const(0x19)   # RW
+_FAULT_STATUS = const(0x00)  # Fault Status Register R-
+_RC_STATUS1 = const(0x01)  # Motor Speed status R-
+_REG_STATUS1 = const(0x04)  # Output voltage across motor R-
+_REG_STATUS2 = const(0x05)  # Current flowing through motor R-
+_REG_STATUS3 = const(0x06)  # PWM Duty Cycle R-
+_CONFIG0 = const(0x09)  # RW
+_CONFIG1 = const(0x0A)  # RW
+_CONFIG2 = const(0x0B)  # RW
+_CONFIG3 = const(0x0C)  # RW
+_CONFIG4 = const(0x0D)  # RW
+_REG_CTRL0 = const(0x0E)  # RW
+_REG_CTRL1 = const(0x0F)  # RW
+_REG_CTRL2 = const(0x10)  # RW
+_RC_CTRL2 = const(0x13)  # RW
+_RC_CTRL3 = const(0x14)  # RW
+_RC_CTRL4 = const(0x15)  # RW
+_RC_CTRL7 = const(0x18)  # RW
+_RC_CTRL8 = const(0x19)  # RW
 
 
 class VoltageAdapter:
@@ -35,20 +33,20 @@ class VoltageAdapter:
 
     def index_to_voltage(self, index):
         """Convert an index value to nearest voltage value."""
-        return index * (42.67/255)
+        return index * (42.67 / 255)
 
     def voltage_to_index(self, volts):
         """Convert a voltage to nearest index value."""
-        return round(volts * (255/42.67))
+        return round(volts * (255 / 42.67))
 
 
 class BridgeControl:
     """H-bridge PWM control states and descriptors. Bit order: IN2 IN1"""
 
-    COAST   = 0b00  # Standby/Coast function (Hi-Z)
+    COAST = 0b00  # Standby/Coast function (Hi-Z)
     REVERSE = 0b01  # Reverse function
     FORWARD = 0b10  # Forward function
-    BRAKE   = 0b11  # Brake function
+    BRAKE = 0b11  # Brake function
 
     DESCRIPTOR = ["COAST", "REVERSE", "FORWARD", "BRAKE"]
 
@@ -69,8 +67,9 @@ class Faults:
 
     DESCRIPTOR = ["FAULT", "OCP", "OVP", "UVLO", "TSD"]
 
-class DRV8235():
-#class DRV8235(Driver):
+
+class DRV8235:
+    # class DRV8235(Driver):
     """DC motor driver with I2C interface.
 
     :param i2c_bus: The microcontroller I2C interface bus pins.
@@ -83,8 +82,8 @@ class DRV8235():
         self._i2c_bc = True
         self._pmode = True
         self._dir = BridgeControl.COAST
-        self._reg_ctrl = 0x3 # Sets to voltage regulation
-        self._wset_vset = 0 # Sets initial voltage to 0
+        self._reg_ctrl = 0x3  # Sets to voltage regulation
+        self._wset_vset = 0  # Sets initial voltage to 0
         self._int_vref = True
         self._inv_r_scale = 0x3
         self._inv_r = 82
@@ -94,26 +93,25 @@ class DRV8235():
 
         super().__init__()
 
-
     # DEFINE I2C DEVICE BITS AND REGISTERS
     _clear = RWBit(_CONFIG0, 1, 1, False)  # Clears fault status flag bits
-    _i2c_bc = RWBit(_CONFIG4, 2, 1, False) # Sets Bridge Control to I2C
-    _pmode = RWBit(_CONFIG4, 3, 1, False) # Sets programming mode to PWM
-    _en_out = RWBit(_CONFIG0, 7, 1, False) # Enables output
-    _dir = RWBits (2, _CONFIG4, 0, 1, False) # Sets direction of h-bridge IN1, IN2
-    _reg_ctrl = RWBits (2, _REG_CTRL0, 3, 1, False) # Sets current/voltage regulation scheme
+    _i2c_bc = RWBit(_CONFIG4, 2, 1, False)  # Sets Bridge Control to I2C
+    _pmode = RWBit(_CONFIG4, 3, 1, False)  # Sets programming mode to PWM
+    _en_out = RWBit(_CONFIG0, 7, 1, False)  # Enables output
+    _dir = RWBits(2, _CONFIG4, 0, 1, False)  # Sets direction of h-bridge IN1, IN2
+    _reg_ctrl = RWBits(2, _REG_CTRL0, 3, 1, False)  # Sets current/voltage regulation scheme
     _fault = ROBit(_FAULT_STATUS, 7, 1, False)  # Any fault condition
-    _stall = ROBit(_FAULT_STATUS, 5, 1, False) # Stall event
-    _ocp = ROBit (_FAULT_STATUS, 4, 1, False) # Overcurrent event
-    _ovp = ROBit (_FAULT_STATUS, 3, 1, False) # Overvoltage event
-    _tsd = ROBit (_FAULT_STATUS, 2, 1, False) # Overtemperature event
-    _npor = ROBit (_FAULT_STATUS, 1, 1, False) # Undervoltage event
-    _wset_vset = RWBits (8, _REG_CTRL1, 0, 1, False) #Sets target motor voltage
+    _stall = ROBit(_FAULT_STATUS, 5, 1, False)  # Stall event
+    _ocp = ROBit(_FAULT_STATUS, 4, 1, False)  # Overcurrent event
+    _ovp = ROBit(_FAULT_STATUS, 3, 1, False)  # Overvoltage event
+    _tsd = ROBit(_FAULT_STATUS, 2, 1, False)  # Overtemperature event
+    _npor = ROBit(_FAULT_STATUS, 1, 1, False)  # Undervoltage event
+    _wset_vset = RWBits(8, _REG_CTRL1, 0, 1, False)  # Sets target motor voltage
     _vmtr = ROBits(8, _REG_STATUS1, 0, 1, False)
-    _imtr = ROBits (8, _REG_STATUS2, 0, 1, False)
+    _imtr = ROBits(8, _REG_STATUS2, 0, 1, False)
     _duty_read = ROBits(6, _REG_STATUS3, 0, 1, False)
-    _int_vref = RWBit (_CONFIG3, 4, 1, False)
-    _inv_r = RWBits (8, _RC_CTRL3, 0, 1, False)
+    _int_vref = RWBit(_CONFIG3, 4, 1, False)
+    _inv_r = RWBits(8, _RC_CTRL3, 0, 1, False)
     _inv_r_scale = RWBits(2, _RC_CTRL2, 6, 1, False)
 
     def throttle(self):
@@ -128,7 +126,6 @@ class DRV8235():
         if self.bridge_control[0] == BridgeControl.REVERSE:
             return -1 * round(self._wset_vset / 0xFF, 3)
         return round(self._wset_vset / 0xFF, 3)
-
 
     def set_throttle(self, new_throttle):
         if new_throttle is None:
@@ -148,7 +145,6 @@ class DRV8235():
             self._dir = BridgeControl.BRAKE
         return
 
-
     def throttle_volts(self):
         """Current motor voltage, ranging from -42.7 volts (full speed reverse) to
         +42.7 volts (full speed forward), or ``None`` (controller off). If ``None``,
@@ -161,7 +157,6 @@ class DRV8235():
         if self.bridge_control[0] == BridgeControl.REVERSE:
             return -1 * VoltageAdapter.index_to_voltage(self, self._wset_vset)
         return VoltageAdapter.index_to_voltage(self, self._wset_vset)
-
 
     def set_throttle_volts(self, new_throttle_volts):
         if new_throttle_volts is None:
@@ -180,7 +175,6 @@ class DRV8235():
             self._wset_vset = 0
             self._dir = BridgeControl.BRAKE
         return
-
 
     def throttle_raw(self):
         """Current motor voltage, 8-bit WSET_VSET byte value, ranging from -255 (full speed reverse) to
@@ -213,13 +207,11 @@ class DRV8235():
             self._dir = BridgeControl.BRAKE
         return
 
-
     @property
     def bridge_control(self):
         """Motor driver bridge status. Returns the 2-bit bridge control integer
         value and corresponding description string."""
         return self._dir, BridgeControl.DESCRIPTOR[self._dir]
-
 
     @property
     def fault(self):
@@ -246,7 +238,6 @@ class DRV8235():
     def __enter__(self):
         return self
 
-
     def __exit__(self, exception_type, exception_value, traceback):
         self._wset_vset = 0
         self._dir = BridgeControl.STANDBY
@@ -254,6 +245,7 @@ class DRV8235():
     """
     ----------------------- HANDLER METHODS -----------------------
     """
+
 
 #     @property
 #     def get_flags(self):
@@ -271,7 +263,7 @@ class DRV8235():
 #                 flags["npor"] = None
 #         return flags
 
-    ######################### DIAGNOSTICS #########################
+######################### DIAGNOSTICS #########################
 
 #     def __check_for_faults(self) -> list[int]:
 #         """_check_for_faults: Checks for any device faluts returned by fault function in DRV8235
