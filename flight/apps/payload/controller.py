5233c45e0aad838d9c737365463c08d14e852bf8
ibrahima <ibrahima.sr.sow@gmail.com>
2025-03-13 02:41:17 -0700

    adding payload python interface backbone

---
+++ flight/apps/payload/controller.py
@@ -0,0 +1,13 @@
+# Payload Control Interface
+
+
+class PayloadState:  # From the host perspective
+    pass
+
+
+class PayloadTM:  # Simple data structure holder
+    pass
+
+
+class PayloadHostControl:
+    pass
