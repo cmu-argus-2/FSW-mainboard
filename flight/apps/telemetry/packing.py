52b94233246c3c49f3c35cac2e0bcbdcf27198fd
Akshat412 <akshat412000@gmail.com>
2025-03-13 11:02:12 -0400

    Fixed length of TM packer for HBs

--- flight/apps/telemetry/packing.py
+++ flight/apps/telemetry/packing.py
@@ -49,12 +49,13 @@ class TelemetryPacker:
 
     _TM_AVAILABLE = False
 
-    _TM_FRAME_SIZE = const(248)  # size of the telemetry frame
+    _TM_FRAME_SIZE = const(248)  # maximum allowed size of the telemetry frame
 
+    # Frame size and packet length SHALL be updated for every TM frame definition
     _FRAME = bytearray(_TM_FRAME_SIZE)  # pre-allocated buffer for packing
     _FRAME[0] = const(0x01) & 0xFF  # message ID
     _FRAME[1:3] = pack_unsigned_short_int([const(0x00)], 0)  # sequence count
-    _FRAME[3] = const(229) & 0xFF  # packet length
+    _FRAME[3] = const(_TM_FRAME_SIZE) & 0xFF  # packet length
 
     @classmethod
     def FRAME(cls):
@@ -81,10 +82,10 @@ class TelemetryPacker:
         if not cls._TM_AVAILABLE:
             cls._TM_AVAILABLE = True
 
-        cls._FRAME = bytearray(227 + 4)  # pre-allocated buffer for packing
+        cls._FRAME = bytearray(229 + 4)  # pre-allocated buffer for packing
         cls._FRAME[0] = const(0x01) & 0xFF  # message ID
         cls._FRAME[1:3] = pack_unsigned_short_int([const(0x00)], 0)  # sequence count
-        cls._FRAME[3] = const(227) & 0xFF  # packet length
+        cls._FRAME[3] = const(229) & 0xFF  # packet length
 
         ############ CDH fields ############
         if DH.data_process_exists("cdh"):
