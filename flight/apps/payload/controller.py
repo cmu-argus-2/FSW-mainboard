19c81c465fc91d826d8598f4a5e4857656381298
ibrahima <ibrahima.sr.sow@gmail.com>
2025-03-17 02:40:02 -0700

    initial payload controller

--- flight/apps/payload/controller.py
+++ flight/apps/payload/controller.py
@@ -1,13 +1,45 @@
-# Payload Control Interface
+"""
+Payload Control Interface
 
+This module defines the Payload Controller class, which is responsible for managing the main interface between
+the host and the Payload.
 
-class PayloadState:  # From the host perspective
-    pass
+Author: Ibrahima Sory Sow
+
+"""
+
+from definitions import CommandID, ErrorCodes
+from protocol import Decoder, Encoder
+
+_PING_VALUE = 0x60
 
 
 class PayloadTM:  # Simple data structure holder
     pass
 
 
-class PayloadHostControl:
-    pass
+class PayloadController:
+
+    communication_interface = None
+
+    @classmethod
+    def initialize(cls, communication_interface):
+        cls.communication_interface = communication_interface
+        cls.communication_interface.connect()
+
+    @classmethod
+    def deinitialize(cls):
+        cls.communication_interface.disconnect()
+
+    @classmethod
+    def ping(cls):
+        cls.communication_interface.send(Encoder.encode_ping())
+
+        resp = cls.communication_interface.receive()
+        if resp:
+            return Decoder.decode(resp) == _PING_VALUE
+        return False
+
+    @classmethod
+    def shutdown(cls):
+        pass
