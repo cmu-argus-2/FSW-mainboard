7a482d3206cfc2b4f6d5e66190c3c1dd666980fb
Karthik Karumanchi <kkaruman@andrew.cmu.edu>
2025-03-21 14:32:53 -0400

    Fixing bug in ECEF2ECI

--- flight/apps/adcs/frames.py
+++ flight/apps/adcs/frames.py
@@ -128,7 +128,6 @@ def convert_ecef_state_to_eci(r: np.ndarray, v: np.ndarray, utime: int):
     r_eci = np.dot(R_ecef2eci, r)
 
     # Transform Velocity
-    r_ecef_surface = (np.linalg.norm(r) - EQUATORIAL_RADIUS * 1e3) * r / np.linalg.norm(r)
-    v_eci = np.dot(R_ecef2eci, v) + np.cross(np.array([0, 0, OMEGA_E]), r_ecef_surface)
+    v_eci = np.dot(R_ecef2eci, v) + np.cross(np.array([0, 0, OMEGA_E]), r)
 
     return r_eci, v_eci
