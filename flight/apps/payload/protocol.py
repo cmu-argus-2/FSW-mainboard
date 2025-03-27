ce9ace53b4d1b7020ab899032a29415a745c85c0
ibrahima <ibrahima.sr.sow@gmail.com>
2025-03-27 00:27:02 -0700

    encoder clear and shutdown cmd

--- flight/apps/payload/protocol.py
+++ flight/apps/payload/protocol.py
@@ -25,7 +25,7 @@ Author: Ibrahima Sory Sow
 
 """
 
-from definitions import CommandID, ErrorCodes
+from definitions import ACK, CommandID, ErrorCodes
 
 # Asymmetric sizes for send and receive buffers
 _RECV_PCKT_BUF_SIZE = 256
@@ -33,92 +33,135 @@ _SEND_PCKT_BUF_SIZE = 32
 
 
 class Encoder:
+
+    # send buffer
     _send_buffer = bytearray(_SEND_PCKT_BUF_SIZE)
-    # TODO: clear buffer before each send
+
+    # Some optimization compatible with CP
+    _bytes_set_last_time = 0
+
+    @classmethod
+    def clear_buffer(cls):
+        # compatible with circuitpython
+        for i in range(cls._bytes_set_last_time):
+            cls._send_buffer[i] = 0
 
     @classmethod
     def encode_ping(cls):
+        cls.clear_buffer()
         cls._send_buffer[0] = CommandID.PING_ACK
+        cls._bytes_set_last_time = 1
         return cls._send_buffer
 
     @classmethod
     def encode_shutdown(cls):
+        cls.clear_buffer()
         cls._send_buffer[0] = CommandID.SHUTDOWN
+        cls._bytes_set_last_time = 1
         return cls._send_buffer
 
     @classmethod
     def encode_synchronize_time(cls):
+        cls.clear_buffer()
         cls._send_buffer[0] = CommandID.SYNCHRONIZE_TIME
+        cls._bytes_set_last_time = 1
         return cls._send_buffer
 
     @classmethod
     def encode_request_telemetry(cls):
+        cls.clear_buffer()
         cls._send_buffer[0] = CommandID.REQUEST_TELEMETRY
+        cls._bytes_set_last_time = 1
         return cls._send_buffer
 
     @classmethod
     def encode_enable_cameras(cls):
+        cls.clear_buffer()
         cls._send_buffer[0] = CommandID.ENABLE_CAMERAS
+        cls._bytes_set_last_time = 1
         return cls._send_buffer
 
     @classmethod
     def encode_disable_cameras(cls):
+        cls.clear_buffer()
         cls._send_buffer[0] = CommandID.DISABLE_CAMERAS
+        cls._bytes_set_last_time = 1
         return cls._send_buffer
 
     @classmethod
     def encode_capture_images(cls):
+        cls.clear_buffer()
         cls._send_buffer[0] = CommandID.CAPTURE_IMAGES
+        cls._bytes_set_last_time = 1
         return cls._send_buffer
 
     @classmethod
     def encode_start_capture_images_periodically(cls):
+        cls.clear_buffer()
         cls._send_buffer[0] = CommandID.START_CAPTURE_IMAGES_PERIODICALLY
+        cls._bytes_set_last_time = 1  # TODO: Add arguments
         return cls._send_buffer
 
     @classmethod
     def encode_stop_capture_images(cls):
+        cls.clear_buffer()
         cls._send_buffer[0] = CommandID.STOP_CAPTURE_IMAGES
+        cls._bytes_set_last_time = 1  # TODO: Add arguments
         return cls._send_buffer
 
     @classmethod
     def encode_stored_images(cls):
+        cls.clear_buffer()
         cls._send_buffer[0] = CommandID.STORED_IMAGES
+        cls._bytes_set_last_time = 1  # TODO: Add arguments
         return cls._send_buffer
 
     @classmethod
     def encode_request_image(cls):
+        cls.clear_buffer()
         cls._send_buffer[0] = CommandID.REQUEST_IMAGE
+        cls._bytes_set_last_time = 1  # TODO: Add arguments
         return cls._send_buffer
 
     @classmethod
     def encode_delete_images(cls):
+        cls.clear_buffer()
         cls._send_buffer[0] = CommandID.DELETE_IMAGES
+        cls._bytes_set_last_time = 1  # TODO: Add arguments
         return cls._send_buffer
 
     @classmethod
     def encode_run_od(cls):
+        cls.clear_buffer()
         cls._send_buffer[0] = CommandID.RUN_OD
+        cls._bytes_set_last_time = 1  # TODO: Add arguments
         return cls._send_buffer
 
     @classmethod
     def encode_ping_od_status(cls):
+        cls.clear_buffer()
         cls._send_buffer[0] = CommandID.PING_OD_STATUS
+        cls._bytes_set_last_time = 1
         return cls._send_buffer
 
     @classmethod
     def encode_debug_display_camera(cls):
+        cls.clear_buffer()
         cls._send_buffer[0] = CommandID.DEBUG_DISPLAY_CAMERA
+        cls._bytes_set_last_time = 1
         return cls._send_buffer
 
     @classmethod
     def encode_debug_stop_display(cls):
+        cls.clear_buffer()
         cls._send_buffer[0] = CommandID.DEBUG_STOP_DISPLAY
+        cls._bytes_set_last_time = 1
         return cls._send_buffer
 
     # Generic example without any checking
     @classmethod
     def encode_with_args(cls, command_id, *args):
+        cls.clear_buffer()
         cls._send_buffer[0] = command_id
         for i, arg in enumerate(args, start=1):
             cls._send_buffer[i] = arg
@@ -163,3 +206,18 @@ class Decoder:
         if cls._curr_data_length != 1:
             return ErrorCodes.INVALID_PACKET
         return int(cls._recv_buffer[cls._data_idx][0])
+
+    @classmethod
+    def decode_shutdown(cls):
+        if cls._curr_data_length != 0:
+            return ErrorCodes.INVALID_PACKET
+
+        resp = int(cls._recv_buffer[cls._data_idx][0])
+
+        if resp == ACK.ERROR:
+            return ErrorCodes.COMMAND_ERROR_EXECUTION
+
+        if resp == ACK.SUCCESS:
+            return ErrorCodes.OK
+        else:
+            return ErrorCodes.INVALID_RESPONSE
