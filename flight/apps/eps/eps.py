13ba815b8494868b736f4804ec62e4580c521576
Ankita Chatterjee <achstar1102004@gmail.com>
2025-03-29 16:51:39 -0400

    Added comments for app functions

--- flight/apps/eps/eps.py
+++ flight/apps/eps/eps.py
@@ -57,8 +57,14 @@ def GET_EPS_POWER_FLAG(curr_flag, soc):
 
 
 def GET_POWER_STATUS(buf, power, threshold, window):
+    """returns whether MAV power is above provided threshold"""
+    # Add this power value to provided MAV buffer
     buf.append(power)
+    # If the length of the buffer is now greater than the window size,
+    # Pop the first (oldest) element of the buffer
     if (len(buf) > window):
         buf.pop(0)
+    # Obtain the moving average
     power_avg = sum(buf) / len(buf)
+    # Return the moving average value & the power status
     return (power_avg >= threshold), power_avg
