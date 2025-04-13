38f5bb9ce7132578c9d3435b2bf4c4dd50092c7f
ibrahima <ibrahima.sr.sow@gmail.com>
2025-04-13 12:36:01 -0700

    init data processes

--- flight/tasks/payload.py
+++ flight/tasks/payload.py
@@ -15,12 +15,23 @@ class Task(TemplateTask):
         super().__init__(id)
         self.name = "PAYLOAD"
 
+    def init_all_data_processes(self):
+        # Image process
+        if not DH.data_process_exists("payload/img"):
+            DH.register_image_process()  # WARNING: Image process from DH is different from regular data processes!
+
+        # Telemetry process
+        if not DH.data_process_exists("payload/tm"):
+            data_format = 3 * "L" + 4 * "B" + 4 * "B" + 4 * "B" + 8 * "B" + 3 * "H"
+            DH.register_data_process(tag_name="payload/tm", data_format=data_format, persistent=True, data_limit=100000)
+
+        # OD process (should be a separate data process)
+        if not DH.data_process_exists("payload/od"):
+            pass
+
     async def main_task(self):
         if SM.current_state == STATES.STARTUP:
             # Grab the communication interface from SATELLITE
             pass
         else:
-            if not DH.data_process_exists("img"):
-                DH.register_image_process()  # WARNING: Image process from DH is different from regular data processes!
-
-            # TODO: other commands and logging here
+            self.init_all_data_processes()
