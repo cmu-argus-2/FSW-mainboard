9be4fd43475c50100cec02132639a5f5756d6521
Akshat412 <akshat412000@gmail.com>
2025-04-17 13:25:03 -0400

    Do not give CMD DOWNLINK_ALL request to commanding

--- flight/tasks/comms.py
+++ flight/tasks/comms.py
@@ -202,7 +202,7 @@ class Task(TemplateTask):
                 # Get most recent payload
                 self.rx_payload = SATELLITE_RADIO.get_rx_payload()
 
-                if self.rq_cmd != MSG_ID.GS_CMD_FILE_PKT:
+                if self.rq_cmd != MSG_ID.GS_CMD_FILE_PKT and self.rq_cmd != MSG_ID.GS_CMD_DOWNLINK_ALL:
                     # Push rq_cmd onto CommandQueue along with all its arguments
                     CommandQueue.overwrite_command(self.rq_cmd, self.rx_payload)
 
