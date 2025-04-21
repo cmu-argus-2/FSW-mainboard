7de923f1a5f4b6feaae1f48c6e54adec63942a75
Perrin <perrintong@cmu.edu>
2025-04-21 10:04:06 -0400

    updated burnwire sequence

--- flight/tasks/command.py
+++ flight/tasks/command.py
@@ -19,7 +19,7 @@ from micropython import const
 
 _TPM_INIT_TIMEOUT = const(10)  # seconds
 _EXIT_STARTUP_TIMEOUT = const(5)  # seconds
-_BURN_WIRE_STRENGTH = const(1)  # 0-255
+_BURN_WIRE_STRENGTH = const(7)  # 0-255
 
 
 class Task(TemplateTask):
@@ -56,9 +56,13 @@ class Task(TemplateTask):
             self.log_info("Burn wires available, deploying...")
             burn_wires = SATELLITE.BURN_WIRES
             burn_wires.set_pwm(0, _BURN_WIRE_STRENGTH)
-            burn_wires.set_pwm(1, _BURN_WIRE_STRENGTH)
-            burn_wires.set_pwm(2, _BURN_WIRE_STRENGTH)
             burn_wires.enable_driver()
+            TPM.sleep(5)
+            burn_wires.set_pwm(0, 0)
+            burn_wires.set_pwm(1, _BURN_WIRE_STRENGTH)
+            TPM.sleep(5)
+            burn_wires.set_pwm(1, 0)
+            burn_wires.set_pwm(2, _BURN_WIRE_STRENGTH)
             TPM.sleep(5)
             burn_wires.disable_driver()
         return
