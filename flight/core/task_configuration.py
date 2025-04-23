cd52dda57059953d581756a48d8ea050c3247d72
ibrahima <ibrahima.sr.sow@gmail.com>
2025-04-23 00:23:31 -0700

    integration tests init

--- flight/core/task_configuration.py
+++ flight/core/task_configuration.py
@@ -13,8 +13,8 @@ TASK_CONFIG = {
     TASK.WATCHDOG: {"Task": watchdog, "Frequency": 1, "Priority": 1},
     TASK.EPS: {"Task": eps, "Frequency": 5, "Priority": 2},
     TASK.OBDH: {"Task": obdh, "Frequency": 0.5, "Priority": 2},
-    TASK.COMMS: {"Task": comms, "Frequency": 1, "Priority": 2, "ScheduleLater": True},
-    TASK.ADCS: {"Task": adcs, "Frequency": 5, "Priority": 1},
-    TASK.GPS: {"Task": gps, "Frequency": 0.025, "Priority": 3, "ScheduleLater": True},
+    # TASK.COMMS: {"Task": comms, "Frequency": 1, "Priority": 2, "ScheduleLater": True},
+    # TASK.ADCS: {"Task": adcs, "Frequency": 5, "Priority": 1},
+    # TASK.GPS: {"Task": gps, "Frequency": 0.025, "Priority": 3, "ScheduleLater": True},
     TASK.PAYLOAD: {"Task": payload, "Frequency": 1, "Priority": 3, "ScheduleLater": True},
 }
