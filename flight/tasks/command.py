79b6ca6083d8f6e77f5687c1be24c5fa00574de4
Perrin <perrintong@cmu.edu>
2025-04-27 15:05:47 -0400

    undid an oopsie

--- flight/tasks/command.py
+++ flight/tasks/command.py
@@ -18,7 +18,7 @@ from hal.configuration import SATELLITE
 from micropython import const
 
 _TPM_INIT_TIMEOUT = const(10)  # seconds
-_EXIT_STARTUP_TIMEOUT = const(30)  # seconds
+_EXIT_STARTUP_TIMEOUT = const(5)  # seconds
 _BURN_WIRE_STRENGTH = const(7)  # 0-255
 _DEPLOYMENT_INTERVAL = const(5)  # seconds
 _PWM_MAX = const(3)  # Maximum PWM value for deployment
