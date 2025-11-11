33c62e91027db23229ff9a95e93d8301d35830b2
Jose Mateus <jose.anca.mateus@tecnico.ulisboa.pt>
2025-11-11 19:55:24 +0000

    feat(main): removed most of the code from main. Added code to init radio and implement simple echo program
    it will print to the terminal the message, rssi and snr. And it will respond back with the same message

--- flight/main.py
+++ flight/main.py
@@ -5,6 +5,13 @@ import time
 from core import logger, setup_logger, state_manager
 from hal.configuration import SATELLITE
 
+from apps.comms.comms import SATELLITE_RADIO
+from micropython import const
+from hal.cubesat import Device
+
+ASIL4 = const(4)
+
+
 
 # Memory stats
 def print_memory_stats(call_gc=True):
@@ -24,35 +31,57 @@ setup_logger(level="INFO")
 
 print_memory_stats(call_gc=True)
 
-print("Booting ARGUS...")
-SATELLITE.boot_sequence()
-print("ARGUS booted.")
-print(f"Boot Errors: {SATELLITE.ERRORS}")
+# print("Booting ARGUS...")
+# # this will start each one of the devices
+# SATELLITE.boot_sequence()
+# print("ARGUS booted.")
+# print(f"Boot Errors: {SATELLITE.ERRORS}")
 
+# lets init only the radio
+radio_device = Device(SATELLITE.__radio_boot, ASIL4, peripheral_line=False)
+SATELLITE.__boot_device("RADIO",  radio_device)
+SATELLITE.__device_list["RADIO"].device = radio_device.device
+print(f"Boot Errors: {radio_device.error}")
 
 print("Waiting 1 sec...")
 time.sleep(1)
 
+# transmit a simple message to indicate that the satellite has started
+tx_msg_id =  SATELLITE_RADIO.transmit_message()
+print(f"Transmitted message with ID: {tx_msg_id}")
 
-"""print("Running system diagnostics...")
-errors = SATELLITE.run_system_diagnostics()
-print("System diagnostics complete")
-print("Errors:", errors)
-"""
 
-print_memory_stats(call_gc=False)
-print_memory_stats(call_gc=True)
+# start listening for messages
+print("Starting RX mode...")
+tx_counter = 0
+while True:
+    if SATELLITE_RADIO.data_available():
+        if SATELLITE.RADIO_AVAILABLE:
+            
+            # get the packet
+            packet, err = SATELLITE.RADIO.recv(len=0, timeout_en=True, timeout_ms=1000)
+            
+            # get the rssi and snr of the packet
+            rssi = SATELLITE.RADIO.rssi()  # this is the value for the last received packet
+            snr = SATELLITE.RADIO.snr()    # this is the value for the last received packet
+            
+            print(f"Received packet: {packet}")
+            print(f"Received error: {err}")
+            print(f"Received RSSI: {rssi}")
+            print(f"Received SNR: {snr}")
+            
+            time.sleep(2)
+            
+            # echo back the packet
+            _, state = SATELLITE.RADIO.send(packet)
+            if state != 0:
+                print(f"Failed to echo back message, state: {state}")
+            tx_counter += 1
+            print(f"Echoed back message with ID: {tx_counter}")
 
-try:
-    # Run forever
+        
 
-    # from core import DataHandler as DH
 
-    # DH.delete_all_files()
 
-    logger.info("Starting state manager")
-    state_manager.start()
-
-except Exception as e:
-    logger.critical("ERROR:", e)
-    # TODO Log the error
+# lets implement a simple echo server with the lora module
+# will respond to any received packet with the same data
