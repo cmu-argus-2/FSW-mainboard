f239857a422b17fd6ac5f283324ee9b110ebd450
Mahii11 <mmesfin@andrew.cmu.edu>
2026-03-06 15:56:52 -0500

    Modified halmonitor reboot

--- flight/tasks/hal_monitor.py
+++ flight/tasks/hal_monitor.py
@@ -195,22 +195,11 @@ class Task(TemplateTask):
 
         self.log_device_status()
         DH.log_data(self.log_name, self.log_data)
+        
         # regular reboot every 24 hours
-        # TODO: delay this if the satellite is at a ground pass
-        # if TPM.monotonic() - SATELLITE.BOOTTIME >= _REGULAR_REBOOT_TIME:
-        #     # TODO: graceful shutdown for payload if needed
-        #     self.log_info("Executing regular reboot")
-        #     self.close_data_process()
-        #     SATELLITE.reboot()
         if (TPM.monotonic() - SATELLITE.BOOTTIME >= _REGULAR_REBOOT_TIME):
             try:
-                latest_ground_pass = DH.get_latest_data("ground_pass")
-                if latest_ground_pass is not None:
-                    ground_pass_status = latest_ground_pass[0]
-                else:
-                    ground_pass_status = False
-
-                if ground_pass_status:
+                if SATELLITE.RADIO_AVAILABLE:
                     self.log_info("Skipping regular reboot due to ground pass")
                 else:
                     self.log_info("Executing regular reboot")
