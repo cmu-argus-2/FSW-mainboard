c53bc85c1ebed3750b7350a1edca3dcf91183ca8
Perrin <perrintong@cmu.edu>
2026-05-08 15:47:23 -0400

    default to ground config

--- flight/core/satellite_config.py
+++ flight/core/satellite_config.py
@@ -1,14 +1,14 @@
-# Auto-generated from flight.yaml
+# Auto-generated from ground.yaml
 # Do not edit - changes will be overwritten by the build system.
 
 from micropython import const
 
 
 class command_config:
-    EXIT_STARTUP_TIMEOUT = const(1800)
-    DETUMBLING_TIMEOUT_DURATION = const(18000)
+    EXIT_STARTUP_TIMEOUT = const(5)
+    DETUMBLING_TIMEOUT_DURATION = const(30)
+    BURN_WIRE_TIMEOUT = const(2)
     PAYLOAD_TESTING_MODE = False
-    BURN_WIRE_TIMEOUT = const(5)
 
 
 class time_processor_config:
@@ -16,19 +16,19 @@ class time_processor_config:
 
 
 class main_config:
-    LOG_LEVEL = "WARNING"
+    LOG_LEVEL = "INFO"
 
 
 class hal_monitor_config:
-    REGULAR_REBOOT = const(86400)
+    REGULAR_REBOOT = const(3600)
 
 
 class comms_config:
-    HB_PERIOD = const(60)
+    HB_PERIOD = const(30)
     AUTH_ENABLED = True
     AUTH_KEY_HEX = "d6172b38acb7d2a28e21662f689d1d15ad78ccc888a9c7a78ef58cb61b0f1e32"
-    SC_CALLSIGN = "CT6ARG"
-    GS_CALLSIGN = "CS5CEP"
+    SC_CALLSIGN = "CT6xxx"
+    GS_CALLSIGN = "CSXXXX"
 
 
 class digipeater_config:
@@ -36,4 +36,4 @@ class digipeater_config:
 
 
 class hal_config:
-    ASIL0_EN = False
+    ASIL0_EN = True
