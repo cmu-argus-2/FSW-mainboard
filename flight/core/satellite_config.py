00c8d877da31fd2fe5f24b86979715c05a2d5e34
perrintong <perrintong@cmu.edu>
2026-05-08 16:09:09 -0400

    disable neopixel for flight (#416)
    * disable asil0 peripherals for flight
    
    * default to ground config

--- flight/core/satellite_config.py
+++ flight/core/satellite_config.py
@@ -27,9 +27,13 @@ class comms_config:
     HB_PERIOD = const(30)
     AUTH_ENABLED = True
     AUTH_KEY_HEX = "d6172b38acb7d2a28e21662f689d1d15ad78ccc888a9c7a78ef58cb61b0f1e32"
-    SC_CALLSIGN = "CT6ARG"
-    GS_CALLSIGN = "CS5CEP"
+    SC_CALLSIGN = "CT6xxx"
+    GS_CALLSIGN = "CSXXXX"
 
 
 class digipeater_config:
     RX_QUEUE_MAX = const(5)
+
+
+class hal_config:
+    ASIL0_EN = True
