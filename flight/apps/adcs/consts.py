ee3cb716e1a336e1dc013ed2150bbd6606245f52
prchall123 <prcachim@gmail.com>
2026-05-14 10:54:40 -0400

    chore(adcs): updating max ang rate limit for controllers

--- flight/apps/adcs/consts.py
+++ flight/apps/adcs/consts.py
@@ -70,8 +70,8 @@ class Modes:
     VF_TUMBLING = 4  # Satellite is tumbling too fast for the ACS to work
 
     # Detumbling
-    VF_TUMBLING_TOL_BDOT = 2.62  # 1.75  # Enter VF tumbling if ω > 1.75 rad/s (100 deg/s)
-    VF_TUMBLING_TOL = 5.24  # 3.49  # Enter VF tumbling if ω > 3.49 rad/s (200 deg/s)
+    VF_TUMBLING_TOL_BDOT = 3.49  # 1.75  # Enter VF tumbling if ω > 3.49 rad/s (200 deg/s)
+    VF_TUMBLING_TOL = 8.72  # 3.49  # Enter VF tumbling if ω > 8.72 rad/s (500 deg/s)
     TUMBLING_TOL = 0.54  # Exit detumbling into stable if ω < 0.54 rad/s (30 deg/s)
 
     # Detumbling only controllers
