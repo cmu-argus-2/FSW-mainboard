00769893b1130dfc656b5e5627efd74053c4672a
prchall123 <prcachim@gmail.com>
2026-05-15 10:31:59 +0100

    adcs mode manager refactoring

--- flight/apps/adcs/modemanager.py
+++ flight/apps/adcs/modemanager.py
@@ -9,11 +9,9 @@ def update_mode(current_mode, ctr_mode, gyro_status, omega, sun_status=None, sun
     """
     - Returns the current mode of the ADCS
     """
-    if ctr_mode in [ControllerModes.BCROSS, ControllerModes.BDOT]:
+    if ctr_mode != ControllerModes.SUN_POINTING:
         return update_mode_detumbling(current_mode, gyro_status, omega)
-    if ctr_mode == ControllerModes.SUN_POINTING:
-        return update_mode_sun_pointing(current_mode, gyro_status, omega, sun_status, sun_pos_body)
-    raise ValueError(f"Invalid Controller Mode {ctr_mode}")
+    return update_mode_sun_pointing(current_mode, gyro_status, omega, sun_status, sun_pos_body)
 
 
 def update_mode_detumbling(current_mode, gyro_status, omega) -> int:
@@ -38,14 +36,13 @@ def update_mode_detumbling(current_mode, gyro_status, omega) -> int:
             return Modes.ACS_OFF
         return Modes.STABLE
 
-    if current_mode == Modes.ACS_OFF:
-        if omega_norm >= Modes.TUMBLING_TOL:
-            return Modes.TUMBLING
-        if omega_norm >= Modes.DETUMBLED_TOL_HI:
-            return Modes.STABLE
-        return Modes.ACS_OFF
-
-    raise ValueError(f"Invalid Current Mode {current_mode}")
+    # if current_mode == Modes.ACS_OFF: -> implicit
+    if omega_norm >= Modes.TUMBLING_TOL:
+        return Modes.TUMBLING
+    if omega_norm >= Modes.DETUMBLED_TOL_HI:
+        return Modes.STABLE
+    return Modes.ACS_OFF
+    # raise ValueError(f"Invalid Current Mode {current_mode}")
 
 
 def update_mode_sun_pointing(current_mode, gyro_status, omega, sun_status, sun_pos_body) -> int:
@@ -63,53 +60,37 @@ def update_mode_sun_pointing(current_mode, gyro_status, omega, sun_status, sun_p
             return Modes.STABLE
         return Modes.TUMBLING
 
+    h = np.dot(ControllerConst.INERTIA_MAT, omega)
+    momentum_error = np.linalg.norm(ControllerConst.INERTIA_MAJOR_DIR - (h / ControllerConst.MOMENTUM_TARGET_MAG))
+
     if current_mode == Modes.STABLE:
-        h_hat = np.dot(ControllerConst.INERTIA_MAT, omega) / ControllerConst.MOMENTUM_TARGET_MAG
-        momentum_error = np.linalg.norm(ControllerConst.INERTIA_MAJOR_DIR - h_hat)
         if omega_norm >= Modes.TUMBLING_TOL:
             return Modes.TUMBLING
         if momentum_error <= Modes.STABLE_TOL_LO and sun_status == StatusConst.OK:
             return Modes.SUN_POINTING
         return Modes.STABLE
 
-    if current_mode == Modes.SUN_POINTING:
-        h_hat = np.dot(ControllerConst.INERTIA_MAT, omega) / ControllerConst.MOMENTUM_TARGET_MAG
-        momentum_error = np.linalg.norm(ControllerConst.INERTIA_MAJOR_DIR - h_hat)
+    if sun_status != StatusConst.OK:
+        return current_mode
+    h_norm = np.linalg.norm(h)
+    if h_norm == 0:
+        return Modes.SUN_POINTING
+    sun_error = np.linalg.norm(sun_pos_body - h / h_norm)
 
+    if current_mode == Modes.SUN_POINTING:
         if momentum_error >= Modes.STABLE_TOL_LO:
             return Modes.STABLE
-
-        if sun_status != StatusConst.OK:
-            return Modes.SUN_POINTING
-
-        h = np.dot(ControllerConst.INERTIA_MAT, omega)
-        h_norm = np.linalg.norm(h)
-        if h_norm == 0:
-            return Modes.SUN_POINTING
-
-        h_hat = h / h_norm  # conical condition
-        sun_error = np.linalg.norm(sun_pos_body - h_hat)
-
-        if sun_status == StatusConst.OK and sun_error <= Modes.SUN_POINTED_TOL_LO:
+        # implicit sun_status == StatusConst.OK
+        if sun_error <= Modes.SUN_POINTED_TOL_LO:
             return Modes.ACS_OFF
 
         return Modes.SUN_POINTING
 
-    if current_mode == Modes.ACS_OFF:
-        h_hat = np.dot(ControllerConst.INERTIA_MAT, omega) / ControllerConst.MOMENTUM_TARGET_MAG
-        momentum_error = np.linalg.norm(ControllerConst.INERTIA_MAJOR_DIR - h_hat)
-        if momentum_error >= Modes.STABLE_TOL_HI:
-            return Modes.STABLE
-        if sun_status == StatusConst.OK:
-            h = np.dot(ControllerConst.INERTIA_MAT, omega)
-            h_norm = np.linalg.norm(h)
-            if h_norm == 0:
-                return Modes.SUN_POINTING
-            h_hat = h / h_norm  # conical condition
-            sun_error = np.linalg.norm(sun_pos_body - h_hat)
-            if sun_error >= Modes.SUN_POINTED_TOL_HI:
-                return Modes.SUN_POINTING
-            return Modes.ACS_OFF
-        return Modes.ACS_OFF
-
-    raise ValueError(f"Invalid Current Mode {current_mode}")
+    # if current_mode == Modes.ACS_OFF: -> implicit
+    if momentum_error >= Modes.STABLE_TOL_HI:
+        return Modes.STABLE
+    # implicit sun_status == StatusConst.OK
+    if sun_error >= Modes.SUN_POINTED_TOL_HI:
+        return Modes.SUN_POINTING
+    return Modes.ACS_OFF
+    # raise ValueError(f"Invalid Current Mode {current_mode}")
