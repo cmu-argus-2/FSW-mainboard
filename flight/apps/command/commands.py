0bb7bfed66da903207de7dd7c7cf8c73cceb64b2
hazcoper <jose.anca.mateus@tecnico.ulisboa.pt>
2026-05-21 16:10:11 +0100

    chore(commands): fsk command temp fix removed the parameters

--- flight/apps/command/commands.py
+++ flight/apps/command/commands.py
@@ -241,26 +241,27 @@ def SET_FSK(freq, power, br, ps, bandwidth, f_dev, p_len, p_detect, sync_len, ad
     logger.warning("Executing SET_FSK: setting modulation to FSK for next transmission")
     
     from hal.configuration import SATELLITE
-    SATELLITE.RADIO.beginFSK(
-        freq=freq,
-        power=power,
-        bR=br,
-        pS=ps,
-        bW=bandwidth,
-        fDev=f_dev,
-        preLength=p_len,
-        preDetect=p_detect,
-        syncLength=sync_len,
-        addrComp=ad_comp,
-        packType=pack_type,
-        plLength=pl_len,
-        crcType=crc_type,
-        whitening=whitening,
-        currentLimit=140.0,
-        tcxoVoltage=1.7,
-        useRegulatorLDO=False,
-        blocking=True
-    )
+    # SATELLITE.RADIO.beginFSK(
+    #     freq=freq,
+    #     power=power,
+    #     bR=br,
+    #     pS=ps,
+    #     bW=bandwidth,
+    #     fDev=f_dev,
+    #     preLength=p_len,
+    #     preDetect=p_detect,
+    #     syncLength=sync_len,
+    #     addrComp=ad_comp,
+    #     packType=pack_type,
+    #     plLength=pl_len,
+    #     crcType=crc_type,
+    #     whitening=whitening,
+    #     currentLimit=140.0,
+    #     tcxoVoltage=1.7,
+    #     useRegulatorLDO=False,
+    #     blocking=True
+    # )
+    SATELLITE.RADIO.beginFSK()
 
     return ["modulation_set_to_fsk"]
 
